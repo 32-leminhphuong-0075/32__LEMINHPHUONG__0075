97beec5a7c683a25d818b7ec7c982be1eecc5658
32-leminhphuong-17a3 <[200~>
2024-01-09 18:47:33 +0700

    first commit

--- dhkl_17a3/chương 10/Bài tập c10 b10.2.py
+++
@@ -1,3 +0,0 @@
-# Tìm |x| bằng cách sử dụng hàm thư viện
-x=int(input('Nhập số nguyên x : '))
-print("Giá trị tuyệt đối của",x,"là:",abs(x))
--- dhkl_17a3/chương 10/Bài tập c10 b10.5.py
+++
@@ -1,7 +0,0 @@
-def kiem_tra_ma_zip(z):
-    if len(z)==6 and z.isdigit()==True:
-        return True
-    else:
-        return False
-z=input("Nhập mã zip :")
-print(kiem_tra_ma_zip(z))
